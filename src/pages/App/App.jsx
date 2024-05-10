8017118bb4f38bf092a1a6499f15ee744d184bd7
Allan Viray <allang.v.dev@gmail.com>
2024-05-10 17:01:08 -0500

    Create NavBar and add Links to OrderHistoryPage and NewOrderPage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from '../../components/NavBar';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
 
   return (
     <main className="App">
       {user ? (
-        <Routes>
-          <Route path="/orders" element={<OrderHistoryPage />} />
-          <Route path="/orders/new" element={<NewOrderPage />} />
-        </Routes>
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+          </Routes>
+        </>
       ) : (
         <AuthPage />
       )}
