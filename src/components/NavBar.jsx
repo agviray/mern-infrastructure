abe21c67a8ff877ac87cc9da15c716ac852ff3c8
Allan Viray <allang.v.dev@gmail.com>
2024-05-14 17:09:13 -0500

    Implement getUser in App.jsx and render user details in Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
+  const { email, name } = user;
   return (
     <nav>
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
+      &nbsp; | &nbsp;
+      <span>Welcome back, {name} </span>
+      &nbsp; | &nbsp;
+      <span>{email}</span>
     </nav>
   );
 };
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
 import './App.css';
 import NavBar from '../../components/NavBar';
 import AuthPage from '../AuthPage/AuthPage';
@@ -7,13 +8,13 @@ import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
       {user ? (
         <>
-          <NavBar />
+          <NavBar user={user} />
           <Routes>
             <Route path="/orders" element={<OrderHistoryPage />} />
             <Route path="/orders/new" element={<NewOrderPage />} />
