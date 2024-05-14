f83bc1ca38b0f6af37263adda5b12d17f3255d75
Allan Viray <allang.v.dev@gmail.com>
2024-05-14 18:28:28 -0500

    Implement log out functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import * as userService from '../utilities/users-service';
 
-const NavBar = ({ user }) => {
-  const { email, name } = user;
+const NavBar = ({ user, setUser }) => {
+  const { name } = user;
+
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
   return (
     <nav>
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
       &nbsp; | &nbsp;
-      <span>Welcome back, {name} </span>
+      <span>Welcome back, {name}!</span>
       &nbsp; | &nbsp;
-      <span>{email}</span>
+      <Link to="" onClick={handleLogOut}>
+        LOG OUT
+      </Link>
     </nav>
   );
 };
