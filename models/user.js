d4276d3ad3a8ee499b8939d439478c70732f8730
Allan Viray <allang.v.dev@gmail.com>
2024-05-14 15:44:31 -0500

    Add code in models/user.js to automatically hash User password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema(
   {
@@ -26,5 +29,14 @@ const userSchema = new Schema(
     },
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
 
 module.exports = mongoose.model('User', userSchema);
