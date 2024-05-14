d84a73bbbf1374496a8a9711e4eae70d71412875
Allan Viray <allang.v.dev@gmail.com>
2024-05-14 17:02:32 -0500

    Create getToken and getUser to obtain token and user object if exists

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -5,3 +5,28 @@ export async function signUp(userData) {
   localStorage.setItem('token', token);
   return token;
 }
+
+export function getToken() {
+  const token = localStorage.getItem('token');
+  // If no token exists, return null.
+  if (!token) {
+    return null;
+  }
+
+  // Get the payload of the token.
+  const payload = JSON.parse(atob(token.split('.')[1]));
+
+  // If token expired, remove it from localStorage.
+  // - Note: A JWT's exp is expressed in seconds, not milliseconds. Therefore, need to convert.
+  if (payload.exp * 1000 < Date.now()) {
+    localStorage.remoteItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  // Return user in the token payuload if token exists, otherwise return null.
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
