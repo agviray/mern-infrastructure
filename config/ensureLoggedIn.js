3819c4b989a11d166472bcde02a4743b0723535f
Allan Viray <allang.v.dev@gmail.com>
2024-05-16 13:33:48 -0500

    Create custom middleware ensureLoggedIn to protect server-side routes

---
+++ config/ensureLoggedIn.js
@@ -0,0 +1,8 @@
+// Protect server side routes
+module.exports = function (req, res, next) {
+  if (!req.user) {
+    return res.status(401).json('Unauthorized');
+  }
+  // Ok to proceed if requestor is a user
+  next();
+};
