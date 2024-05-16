830dd27a6e00a73a5fca1e217e05fcb0dd92ed1f
Allan Viray <allang.v.dev@gmail.com>
2024-05-15 21:34:32 -0500

    Mount custom checkToken middleware in server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Middlware to verify token and assign user object of payload to req.user
+app.use(require('./config/checkToken'));
+
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 
