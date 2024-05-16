fe7e7b1dd7ea0540b8a28855c587773be6d225ea
Allan Viray <allang.v.dev@gmail.com>
2024-05-15 21:11:12 -0500

    Refactor sendRequest to include JWT with server request, if JWT exists

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,3 +1,5 @@
+import { getToken } from './users-service';
+
 export default async function sendRequest(url, method = 'GET', payload = null) {
   // Fetch accepts an options object as the 2nd argument
   // used to include a data payload, set headers, etc.
@@ -6,6 +8,12 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     options.headers = { 'Content-Type': 'application/json' };
     options.body = JSON.stringify(payload);
   }
+  const token = getToken();
+  if (token) {
+    options.headers = options.headers || {};
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+
   const res = await fetch(url, options);
   // res.ok will be false if the status code set is not 2xx
   if (res.ok) return res.json();
